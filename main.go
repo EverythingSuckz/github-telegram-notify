f63c3113cff7e28f11763fef007376ea329fa3e8
wrench <me@jyothis.gq>
2022-08-01 15:38:30 +0530

    REFACTOR: minor tweaks

--- main.go
+++ main.go
@@ -48,10 +48,12 @@ func main() {
 	tg_token := os.Getenv("INPUT_BOT_TOKEN")
 	chatId := os.Getenv("INPUT_CHAT_ID")
 	gitEventRaw := os.Getenv("INPUT_GIT_EVENT")
-
+	print(gitEventRaw)
 	var gitEvent Metadata
-	_ = json.Unmarshal([]byte(gitEventRaw), &gitEvent)
-
+	err := json.Unmarshal([]byte(gitEventRaw), &gitEvent)
+	if err != nil {
+		panic(err)
+	}
 	req_url, _ := url.Parse(fmt.Sprint(apiBaseUri, "/bot", tg_token, "/sendMessage"))
 	params := url.Values{}
 	text := fmt.Sprintf("<b>🔨 %d New commit to</b> <a href=\"%s\">%s</a>[<code>%s</code>]\n\n", len(gitEvent.Event.Commits), gitEvent.repoUrl(), gitEvent.RepositoryName, gitEvent.Ref_name)
@@ -62,11 +64,14 @@ func main() {
 	params.Set("text", text)
 	params.Set("parse_mode", "html")
 	params.Set("disable_web_page_preview", "true")
-	kyb, _ := json.Marshal(map[string][][]map[string]string{
+	kyb, err := json.Marshal(map[string][][]map[string]string{
 		"inline_keyboard": {
 			{{"text": "Open changes", "url": gitEvent.Event.Compare}},
 		},
 	})
+	if err != nil {
+		panic(err)
+	}
 	params.Set("reply_markup", string(kyb))
 	req_url.RawQuery = params.Encode()
 	http.Get(req_url.String())
