ce582cc20d7cda419c61910dc91fa8ff7e443c92
wrench <me@jyothis.gq>
2022-10-16 10:26:16 +0530

    ADD: unit test for commit message testing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github-telegram-notify
 
 go 1.18
+
+require github.com/joho/godotenv v1.4.0
---
+++ main_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"github-telegram-notify/types"
+	"github-telegram-notify/utils"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	dotenv "github.com/joho/godotenv"
+)
+
+func loadEnvs(t *testing.T) (string, string) {
+	err := dotenv.Load()
+	if err != nil {
+		t.Fatal("Error loading .env file")
+	}
+	tg_token := os.Getenv("BOT_TOKEN")
+	if tg_token == "" {
+		t.Fatal("Bot token not specified in .env file")
+	}
+	chatID := os.Getenv("CHAT_ID")
+	if chatID == "" {
+		t.Fatal("Chat ID not specified in .env file")
+	}
+	return tg_token, chatID
+}
+
+func parse(t *testing.T, rawData []byte) (string, string, string) {
+	var gitEvent *types.Metadata
+	err := json.Unmarshal([]byte(rawData), &gitEvent)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	text, markupText, markupUrl, err := utils.CreateContents(gitEvent)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return text, markupText, markupUrl
+}
+
+func TestCommitMessage(t *testing.T) {
+	token, chatID := loadEnvs(t)
+	data, err := ioutil.ReadFile("events/commit.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	text, markupText, markupUrl := parse(t, data)
+	error := utils.SendMessage(token, chatID, text, markupText, markupUrl)
+	if error.Description != "" {
+		t.Fatal(error.String())
+	}
+}
